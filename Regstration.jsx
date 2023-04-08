4666e9c13ab0345c870b1a8ac5133d887be7629b
Set-all-trust <aslan.khamidov96@mail.ru>
2023-04-08 12:31:34 +0500

    uyga vazifa 2

--- Regstration.jsx
+++ Regstration.jsx
@@ -21,6 +21,12 @@ class Reg extends Component {
     const onChangePass = (e) => {
       this.setState((prev) => ({ ...prev, password: e.target.value }));
     };
+    const secondName = (e) => {
+      this.setState((prev) => ({ ...prev, lastname: e.target.value }));
+    };
+    const email = (e) => {
+      this.setState((prev) => ({ ...prev, email: e.target.value }));
+    };
     return (
       <div className="cardstyle">
         <h1>Sign Up</h1>
@@ -29,10 +35,10 @@ class Reg extends Component {
           <input type="text" placeholder="First name" onChange={firstName} />
 
           <label>Last name</label>
-          <input type="text" placeholder="Last name" />
+          <input type="text" placeholder="Last name" onChange={secondName} />
 
           <label>Email adress</label>
-          <input type="email" placeholder="Email adress" />
+          <input type="email" placeholder="Email adress" onChange={email} />
 
           <label>Password</label>
           <input
@@ -47,6 +53,9 @@ class Reg extends Component {
           Already registered? <a href="#">Sign in?</a>
         </p>
         <p>{this.state.firstname}</p>
+        <p>{this.state.lastname}</p>
+        <p>{this.state.email}</p>
+        <p>{this.state.password}</p>
       </div>
     );
   }
