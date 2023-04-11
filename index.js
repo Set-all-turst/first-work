83fd682f987aa2c0eb3db889143f348027b95775
Set-all-trust <aslan.khamidov96@mail.ru>
2023-04-11 08:33:12 +0500

    asd

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Task from "./task";
-import Task2 from "./task2.js";
-import Task3 from "./task3";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Task />
-    <Task2 />
-    <Task3 />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
